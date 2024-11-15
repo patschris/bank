9177efaf9681d7301ce3bfb73a14893d8d9103cc
Christos Patsouras <chpatsouras@gmail.com>
2024-11-15 20:22:47 +0200

    Update Transactions entity

--- src/main/java/com/trial/entities/Transactions.java
+++ src/main/java/com/trial/entities/Transactions.java
@@ -4,9 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 @Entity
@@ -21,9 +24,10 @@ public class Transactions {
 	@ManyToOne
 	private Accounts accounts;
 
-	private double amount;
+	private BigDecimal amount;
 
 	private String type;
 
+	@Temporal(TemporalType.DATE)
 	private Date date;
 }
